7c5aefd3fb95fa7c292dd0dd924f75cef2cacded
Kevin <kwburnett@gmail.com>
2022-06-09 12:34:02 -0500

    fix: update environment variable names

--- export-db.sh
+++ export-db.sh
@@ -1,1 +1,1 @@
-PGPASSWORD=$DB_ADMIN_PASS pg_dump -h localhost -p $POSTGRES_PORT -U postgres -Fc $DB_NAME > idempiere-db.dmp
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h localhost -p $POSTGRES_PORT -U postgres -Fc $DB_NAME > idempiere-db.dmp
