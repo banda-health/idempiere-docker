9afdef45cab524548ae5036299485e1555980a51
Kevin <kwburnett@gmail.com>
2022-06-30 09:57:29 -0500

    feat: allow compose file to be specified in DB export script

--- export-db.sh
+++ export-db.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 export $(grep -v '^#' .env | grep -v '^$' | xargs)
 
-docker compose exec -ti idempiere sh -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U postgres -Fc $DB_NAME > idempiere-db.dmp"
-docker compose cp idempiere:idempiere-db.dmp idempiere-db.dmp
+FILE=docker-compose.yml
+if ! [ -z $1 ]; then
+	FILE=$1
+fi
+
+echo "Using compose file $FILE..."
+
+docker compose -f $FILE exec -ti idempiere sh -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U postgres -Fc $DB_NAME > idempiere-db.dmp"
+docker compose -f $FILE cp idempiere:idempiere-db.dmp idempiere-db.dmp
